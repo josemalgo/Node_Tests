f01c88e6b06291b85509ed56a2e9f0889680fa6b
jose1432@gmail.com <jose1432@hotmail.com>
2022-09-07 12:35:27 +0200

    change n1e2 toBe by toEqual

--- _tests_/N1E2.test.js
+++ _tests_/N1E2.test.js
@@ -4,5 +4,5 @@ it('works with async await', async () => {
     expect.assertions(1);
     const employee = await N1E2.getEmployee(1);
     const salary = N1E2.getSalary(employee);
-    expect(salary).toBe(4000);
+    expect(salary).toEqual(4000);
 });
